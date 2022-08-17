677f86565a2e99f0c16dbb8cc176d563c999cad2
Pierre Jacobs <pierre.jacobs852@gmail.com>
2022-08-17 18:38:47 +0200

    Adapt colors to image colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import './App.scss';
 import { Provider } from 'inversify-react';
 import { createContainer } from './lib/Container';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { addSdkToBody } from './addSdkToBody';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Vibrant from 'node-vibrant';
 import { faBackwardStep, faCirclePlay, faForwardStep, faGear, faMagnifyingGlass, faRepeat, faShuffle, faVolumeHigh } from '@fortawesome/free-solid-svg-icons';
 
 export function App() {
   useEffect(addSdkToBody, []);
+  const imageUrl = "https://i.scdn.co/image/ab67616d00001e02e07e6833b024bdb852b96338";
+  const [color, setColor] = useState('');
+  const [textColor, setTextColor] = useState('');
+
+  useEffect(() => {
+    Vibrant.from(imageUrl).getPalette()
+      .then((val) => {
+        setTextColor(val.LightVibrant?.getHex()!);
+        setColor(val.DarkVibrant?.getHex()!);
+        document.body.style.setProperty('--text', val.Vibrant?.getHex()!);
+      })
+  }, []);
 
   return (
-    <div  style={{
-      width: 400,
-      height: 150,
-      background: "var(--background)",
-      padding: "1em"
-    }}>
-      <Provider container={createContainer}>
+    <Provider container={createContainer}>
+      <div className="background"/>
+      <div className="background-image" style={{
+        backgroundImage: `
+        linear-gradient(to bottom, ${textColor}44, ${color}),
+        url("${imageUrl}")`
+      }}/>
+      <div className="foreground">
         <header>
           <FontAwesomeIcon icon={faMagnifyingGlass} size="lg"/>
-          <span>My title</span>
+          <span className="bold">My title</span>
           <FontAwesomeIcon icon={faGear} size="lg"/>
         </header>
         <main>
@@ -42,7 +56,7 @@ export function App() {
             <input type="range" className="slider audio-player-volume" min={0} max={100}/>
           </div>
         </main>
-      </Provider>
-    </div>
+      </div>
+    </Provider>
   )
 }
